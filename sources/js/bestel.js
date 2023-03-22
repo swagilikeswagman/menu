363286932215b91a66be5109d3dbb54dc0c13353
Twan <71022124+twan102018@users.noreply.github.com>
2023-03-22 22:36:28 +0000

    menu

--- sources/js/bestel.js
+++ sources/js/bestel.js
@@ -1,42 +1,93 @@
 let products = {
-    pizza_contadina: {count: 0, price: 0},
-    pizza_margherita: {count: 0, price: 0},
-    pizza_delizioza: {count: 0, price: 0},
-    pizza_salame:{count: 0, price: 0},
-    appel: {count: 0, price: 0},
-    peer: {count: 0, price: 0},
-    egel: {count: 0, price: 0},
-  };
-  function koopProduct(product, prijs) {
-    products[product].count++;
+  pizza_contadina: { count: 0, price: 0 },
+  pizza_margherita: { count: 0, price: 0 },
+  pizza_delizioza: { count: 0, price: 0 },
+  pizza_salame: { count: 0, price: 0 },
+  pizza_tonno: { count: 0, price: 0 },
+  pizza_rustica: { count: 0, price: 0 },
+  pizza_quattro_formaggi: { count: 0, price: 0 },
+  pizza_di_lusso_margherita: { count: 0, price: 0 },
+  appetizer_pane_con_olio: { count: 0, price: 0 },
+  appetizer_pane_con_tre_crème: { count: 0, price: 0 },
+  appetizer_bruschetta_tomaat: { count: 0, price: 0 },
+  appetizer_caprese: { count: 0, price: 0 },
+  appetizer_carpaccio: { count: 0, price: 0 },
+  appetizer_misto: { count: 0, price: 0 },
+}
+
+function koopProduct(product, prijs) {
+  products[product].count++;
+  products[product].price = products[product].count * prijs;
+  document.getElementById(`totaal_${product}`).value = products[product].count;
+  document.getElementById(`totaalPrijs_${product}`).value =
+    products[product].price.toFixed(2);
+}
+
+function verwijderProduct(product, prijs) {
+  if (products[product].count > 0) {
+    products[product].count--;
     products[product].price = products[product].count * prijs;
-    document.getElementById(`totaal_${product}`).value = products[product].count;
-    document.getElementById(`totaalPrijs_${product}`).value = products[product].price.toFixed(2);
+    document.getElementById(`totaal_${product}`).value =
+      products[product].count;
+    document.getElementById(`totaalPrijs_${product}`).value =
+      products[product].price.toFixed(2);
   }
+}
 
-  function verwijderProduct(product, prijs) {
+function toonProducten() {
+  let producten = "";
+  for (let product in products) {
     if (products[product].count > 0) {
-      products[product].count--;
-      products[product].price = products[product].count * prijs;
-      document.getElementById(`totaal_${product}`).value = products[product].count;
-      document.getElementById(`totaalPrijs_${product}`).value = products[product].price.toFixed(2);
+      producten += `<br>${product.replace("_", " ")}: ${
+        products[product].count
+      }`;
     }
   }
+  return producten;
+}
 
-  function toonProducten() {
-    let producten = "";
-    for (let product in products) {
-      if (products[product].count > 0) {
-          producten += `<br>${product.replace("_", " ")}: ${products[product].count}`;
-      }
-    }
-    return producten;
+function berekenPrijs() {
+  let totaalPrijs = Object.values(products).reduce(
+    (acc, curr) => acc + curr.price,
+    0
+  );
+  document.getElementById("totaalPrijs").value = "€" + totaalPrijs.toFixed(2);
+  document.getElementById(
+    "lijst_producten"
+  ).innerHTML = `Geselecteerde producten:\n${toonProducten()}`;
+}
+
+// expand/retract
+// expand/retract
+
+// function toggleSection(sectionId, button) {
+//   const section = document.querySelector(`#${sectionId}`);
+//   section.classList.toggle('active');
+//   if (section.style.display === 'none') {
+//     section.style.display = 'flex';
+//     button.innerText = '-';
+//   } else {
+//     section.style.display = 'none';
+//     button.innerText = '+';
+//   }
+// }
+
+function toggleSection(sectionId, button) {
+  const section = document.querySelector(`#${sectionId}`);
+  section.classList.toggle("active");
+  if (section.style.display === "none") {
+    section.style.display = "flex";
+    button.innerText = "-";
+  } else {
+    section.style.display = "none";
+    button.innerText = "+";
   }
-  
-  
-  
-  function berekenPrijs() {
-    let totaalPrijs = Object.values(products).reduce((acc, curr) => acc + curr.price, 0);
-    document.getElementById('totaalPrijs').value = "€" + totaalPrijs.toFixed(2);
-    document.getElementById("lijst_producten").innerHTML = (`Geselecteerde producten:\n${toonProducten()}`);
-  }
+  section.addEventListener(
+    "transitionend",
+    () => {
+      section.style.height = "";
+    },
+    { once: true }
+  );
+  section.style.height = `${section.scrollHeight}px`;
+}
