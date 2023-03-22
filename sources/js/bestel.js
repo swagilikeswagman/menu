4760c69d0024bd0322cf65c7525d3cd78e53a762
Twan <71022124+twan102018@users.noreply.github.com>
2023-03-22 14:52:20 +0100

    Menu

--- sources/js/bestel.js
+++ sources/js/bestel.js
@@ -1,7 +1,11 @@
 let products = {
     pizza_contadina: {count: 0, price: 0},
     pizza_margherita: {count: 0, price: 0},
-    bananen: {count: 0, price: 0}
+    pizza_delizioza: {count: 0, price: 0},
+    pizza_salame:{count: 0, price: 0},
+    appel: {count: 0, price: 0},
+    peer: {count: 0, price: 0},
+    egel: {count: 0, price: 0},
   };
   function koopProduct(product, prijs) {
     products[product].count++;
@@ -19,7 +23,20 @@ let products = {
     }
   }
 
+  function toonProducten() {
+    let producten = "";
+    for (let product in products) {
+      if (products[product].count > 0) {
+          producten += `<br>${product.replace("_", " ")}: ${products[product].count}`;
+      }
+    }
+    return producten;
+  }
+  
+  
+  
   function berekenPrijs() {
     let totaalPrijs = Object.values(products).reduce((acc, curr) => acc + curr.price, 0);
-    document.getElementById('totaalPrijs').value = totaalPrijs.toFixed(2);
-}
+    document.getElementById('totaalPrijs').value = "€" + totaalPrijs.toFixed(2);
+    document.getElementById("lijst_producten").innerHTML = (`Geselecteerde producten:\n${toonProducten()}`);
+  }
